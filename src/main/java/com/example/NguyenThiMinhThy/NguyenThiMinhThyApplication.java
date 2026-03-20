91162f0b67279b6f9efe5cef7b1bff59cea6fa33
Nguyễn Thị Minh Thy <minhthy112233@gmail.com>
2026-03-20 10:24:09 +0700

    init: upload project

---
+++ src/main/java/com/example/NguyenThiMinhThy/NguyenThiMinhThyApplication.java
@@ -0,0 +1,13 @@
+package com.example.NguyenThiMinhThy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenThiMinhThyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenThiMinhThyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NguyenThiMinhThy/NguyenThiMinhThyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NguyenThiMinhThy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenThiMinhThyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
